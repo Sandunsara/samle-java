25c5ff904ef01c2315def7e593a7caa2499097bd
Sandunsara <sandunsara2002@gmail.com>
2024-05-11 15:43:25 +0530

    first commit

---
+++ Project_Sample/src/main/java/com/lk/project_sample/Project_Sample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lk.project_sample;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Project_Sample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
